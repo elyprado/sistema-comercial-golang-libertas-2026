7ff362a58d7ed63d895c95c6c79f6a02df665bea
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2026-05-22 15:42:20 -0300

    feat: configuração de CORS

--- go.mod
+++ go.mod
@@ -8,4 +8,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
-require filippo.io/edwards25519 v1.2.0 // indirect
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
 	"go-crud-api/routers"
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	router := routers.SetupRouter()
+
+	c := cors.New(cors.Options{
+        AllowedOrigins:   []string{"http://localhost:8080"}, 
+        AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+        AllowedHeaders:   []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        ExposedHeaders:   []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           43200, 
+    })
+
+	handler := c.Handler(router)
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
