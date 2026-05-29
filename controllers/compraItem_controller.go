42e6c36f737faab3fe411ea0a7ac9ca78a57d7e5
Ulisses Soares <007259@libertas.edu.br>
2026-05-29 20:49:34 -0300

    Implementação metodo GetByIt para CompraItem

--- controllers/compraItem_controller.go
+++ controllers/compraItem_controller.go
@@ -5,6 +5,8 @@ import (
 	"go-crud-api/config"
 	"go-crud-api/models"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Controller para a entidade CompraItem, responsável por lidar com as requisições relacionadas a itens de compra, como criar, ler, atualizar e deletar registros de itens de compra no banco de dados. Cada função se conecta ao banco de dados, executa a operação necessária e retorna a resposta apropriada ao cliente.
@@ -37,6 +39,7 @@ func GetCompraItem(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(compraItems)
 }
 
+// GetCompraItemById é responsável por buscar um item de compra específico no banco de dados com base no ID fornecido na URL. Ele se conecta ao banco de dados, executa uma consulta SQL para selecionar os campos relevantes da tabela de itens de compra onde o ID corresponde ao valor fornecido, e depois codifica o resultado em JSON para enviar de volta ao cliente.
 func GetCompraItemById(w http.ResponseWriter, r *http.Request) {
 	db, err := config.Connect()
 	if err != nil {
@@ -44,6 +47,25 @@ func GetCompraItemById(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	rows, err := db.Query("SELECT idcompraitem, idcompra, idproduto, quantidade, custo_unitario, custo_total FROM compraItem WHERE idcompraitem = ?", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	var compraItem models.CompraItem
+	for rows.Next() {
+		if err := rows.Scan(&compraItem.IdCompraItem, &compraItem.IdCompra, &compraItem.IdProduto, &compraItem.Quantidade, &compraItem.CustoUnitario, &compraItem.CustoTotal); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(compraItem)
 }
 
 func CreateCompraItem(w http.ResponseWriter, r *http.Request) {
