b9e426d4ed533e18f25f0e8f4c741b22bd9bae96
Ulisses Soares <164263005+IHCATI-12@users.noreply.github.com>
2026-06-17 19:56:08 -0300

    Adição das funções para o front-end, get de fornecedores e produtos

--- .history/routers/router_compras_20260615205324.go
+++ .history/routers/router_compras_20260615205324.go
@@ -7,10 +7,6 @@ import (
 )
 
 func SetupComprasRouters(router *mux.Router) {
-	// Rota para retornar fornecedores e produtos para a criação de compras
-	router.HandleFunc("/compras/fornecedores", controllers.GetFornecedoresDisponiveis).Methods("GET")
-	router.HandleFunc("/compras/produtos", controllers.GetProdutosDisponiveis).Methods("GET")
-
 	router.HandleFunc("/compras", controllers.GetCompras).Methods("GET")
 	router.HandleFunc("/compras/{id}", controllers.GetComprasById).Methods("GET")
 	router.HandleFunc("/compras", controllers.CreateCompras).Methods("POST")
--- .history/routers/router_compras_20260617194356.go
+++ .history/routers/router_compras_20260617194356.go
@@ -7,13 +7,13 @@ import (
 )
 
 func SetupComprasRouters(router *mux.Router) {
-	// Rota para retornar fornecedores e produtos para a criação de compras
-	router.HandleFunc("/compras/fornecedores", controllers.GetFornecedoresDisponiveis).Methods("GET")
-	router.HandleFunc("/compras/produtos", controllers.GetProdutosDisponiveis).Methods("GET")
-
 	router.HandleFunc("/compras", controllers.GetCompras).Methods("GET")
 	router.HandleFunc("/compras/{id}", controllers.GetComprasById).Methods("GET")
 	router.HandleFunc("/compras", controllers.CreateCompras).Methods("POST")
 	router.HandleFunc("/compras/{id}", controllers.UpdateCompras).Methods("PUT")
 	router.HandleFunc("/compras/{id}", controllers.DeleteCompras).Methods("DELETE")
+
+	// Rota para retornar fornecedores e produtos para a criação de compras
+	router.HandleFunc("/compras/fornecedores", controllers.GetFornecedoresDisponiveis).Methods("GET")
+	router.HandleFunc("/compras/produtos", controllers.GetProdutosDisponiveis).Methods("GET")
 }
